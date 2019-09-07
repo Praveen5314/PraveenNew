8d209c18faac4129bdeffe970262541f5f3a61fc
Praveen5314 <praveen5314@gmail.com>
2019-09-07 07:29:16 +0530

    Added OddNum class

---
+++ src/BasicPrograms/OddNum.java
@@ -0,0 +1,16 @@
+package BasicPrograms;
+
+public class OddNum {
+
+	public static void main(String[] args) {
+		System.out.println("The Odd Numbers are:");
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	
+
+	}
+
+}
